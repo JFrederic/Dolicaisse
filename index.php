76e27bf63ee311f3a8cf779fde326fb06b80675b
Mobicaisse <frederic.jouan@mobisoft.re>
2025-06-24 18:06:46 +0400

    update

--- index.php
+++ index.php
@@ -19,19 +19,28 @@ require_once 'template/includes.php';
                     <div class="row main-caisse" >
                         <div class="col-9 bg-light text-white ">
                         <div class="row">
-                            <div class="col-3 bg-primary  bloc-dolibarr" data-bs-toggle="modal" data-bs-target="#clientModal">
+                            <div class="col-12 col-md-3 sm bg-primary  bloc-dolibarr" data-bs-toggle="modal" data-bs-target="#clientModal">
                                 <h3 class="text-center"><i class="fa-solid fa-user"></i> Client </h3>
                             </div>
-                            <div class="col-3 bg-danger bloc-dolibarr" data-bs-toggle="modal" data-bs-target="#factureModal">
+                            <div class="col-12 col-md-3 bg-danger bloc-dolibarr" data-bs-toggle="modal" data-bs-target="#factureModal">
                                 <h3 class="text-center"><i class="fa-solid fa-file-invoice"></i> Facture </h3>
                             </div>
-                            <div class="col-3 bg-success bloc-dolibarr">
+                            <div class="col-12 col-md-3 bg-success bloc-dolibarr">
                                 <h3 class="text-center"><i class="fa-solid fa-box"></i> Cloture Caisse </h3>
                             </div>
-                            <div class="col-3 bg-dark bloc-dolibarr">
+                            <div class="col-12 col-md-3 bg-dark bloc-dolibarr">
                                 <h3 class="text-center"><i class="fa-solid fa-cash-register"></i> Total Caisse </h3>
                             </div>
                         </div>
+                        <!-- Panier Composant -->
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="container mt-3">
+                                    <?php require_once 'template/composant/panier.php'; ?>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Fin Panier Composant -->
                     </div>
                     <!-- Colonne droite -->
                     <div class="col-3 bg-primary d-flex align-items-center justify-content-center">
---
+++ template/composant/panier.php
@@ -0,0 +1,29 @@
+<div class="card caddie">
+  <div class="card-header">
+    <h5 class="card-title"> <i class="fa-solid fa-cart-shopping"></i> Caddie 1</h5>
+    <button type="button" class="btn btn-danger btnMultiplePanier" >Client Suivant</button>
+  </div>
+  <div class="card-body">
+    <div class="row">
+        <div class="col-12">
+            <table class="table table-striped table-hover">
+            <thead style="border-bottom: 2px solid lightgray;">
+                <tr>
+                <th scope="col" class="col-designation">Désignation</th>
+                <th scope="col">QTE</th>
+                <th scope="col">PU €</th>
+                <th scope="col">Montant €</th>
+                <th scope="col">Remise %</th>
+                <th scope="col">Remise €</th>
+                </tr>
+            </thead>
+            
+            <tbody id="panierItems">
+                
+                <!-- Les lignes du panier seront ajoutées ici dynamiquement -->
+            </tbody>
+            </table>
+        </div>
+    </div>
+  </div>
+</div>
