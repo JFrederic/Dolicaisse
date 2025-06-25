9ec1ccfd2a1f48d423fbd9faf45f5d767e3a04c2
JFrederic <frederic.jouan@mobisoft.re>
2025-06-25 12:38:48 +0400

    maj ux

---
+++ template/composants/panier.php
@@ -0,0 +1,30 @@
+<div class="card caddie">
+  <div class="card-header">
+    <h5 class="card-title"> <i class="fa-solid fa-cart-shopping"></i> Caddie 1</h5>
+    <button type="button" class="btn btn-danger btnMultiplePanier" >Client Suivant</button>
+  </div>
+  <div class="card-body">
+    <div class="row">
+        <div class="col-12">
+            <table class="table" style="padding: 0 10px;">
+            <thead>
+                <tr>
+                <th scope="col" class="col-designation entete">Désignation</th>
+                <th scope="col" class="entete">QTE</th>
+                <th scope="col" class="entete">PU €</th>
+                <th scope="col" class="entete">Montant €</th>
+                <th scope="col" class="entete">Remise %</th>
+                <th scope="col" class="entete">Remise €</th>
+                </tr>
+            </thead>
+            
+            <tbody id="panierItems">
+                
+                <!-- Les lignes du panier seront ajoutées ici dynamiquement -->
+            </tbody>
+            </table>
+        </div>
+    </div>
+  </div>
+  
+</div>
