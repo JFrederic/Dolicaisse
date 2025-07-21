ce4104fecc49269de71a12ca2396d545bc5aafa5
Mobicaisse <frederic.jouan@mobisoft.re>
2025-07-21 19:31:43 +0400

    maj

--- js/login.js
+++ js/login.js
@@ -17,7 +17,7 @@ function loginMagasin() {
             localStorage.setItem('entrepot_id', res.entrepot_id);
             document.getElementById('loginMsg').innerHTML = '<span class="text-success">Connexion réussie (entrepôt ' + res.entrepot_id + ')</span>';
             // Rediriger ou afficher la caisse
-            window.location.href = 'http://localhost/Dolicaisse/index.php';
+            window.location.href = window.origin + '/Dolicaisse/index.php';
         } else {
             document.getElementById('loginMsg').innerHTML = '<span class="text-danger">Code invalide</span>';
         }
